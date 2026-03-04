15bfc1f95468126162affec22de7879c4daa152e
Reuben Brooks <reuben.brooks@facilitygrid.com>
2026-03-04 11:38:07 -0600

    fix: correct apiurl SecretRef case in ConnectionDetailsMapping
    The generated mapping used 'apiUrlSecretRef' (camelCase) but the JSON
    tag is 'apiurlSecretRef' (lowercase). This prevented upjet from finding
    the SecretKeySelector, so api_url was never injected into TF params.

--- apis/cluster/notificationchannel/v1alpha1/zz_notificationchannel_terraformed.go
+++ apis/cluster/notificationchannel/v1alpha1/zz_notificationchannel_terraformed.go
@@ -21,7 +21,7 @@ func (mg *NotificationChannel) GetTerraformResourceType() string {
 
 // GetConnectionDetailsMapping for this NotificationChannel
 func (tr *NotificationChannel) GetConnectionDetailsMapping() map[string]string {
-	return map[string]string{"slack_configs[*].api_url": "slackConfigs[*].apiUrlSecretRef"}
+	return map[string]string{"slack_configs[*].api_url": "slackConfigs[*].apiurlSecretRef"}
 }
 
 // GetObservation of this NotificationChannel
